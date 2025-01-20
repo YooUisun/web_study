71150ce05380c2594ff6adbd12c31b67f75760ac
YooUisun <yoouisun33@naver.com>
2025-01-20 16:55:09 +0900

    DB연동  room 정보저장,mybatis 설정 진행

--- src/main/java/com/app/controller/admin/AdminController.java
+++ src/main/java/com/app/controller/admin/AdminController.java
@@ -11,31 +11,30 @@ import com.app.service.user.UserService;
 
 @Controller
 public class AdminController {
-	
+
 	@Autowired
 	RoomService roomService;
-	
+
 	@Autowired
 	UserService userService;
 
-	//객실 등록
+	// 객실 등록
 	@GetMapping("/admin/registerRoom")
 	public String registerRoom() {
 		return "admin/registerRoom";
 	}
-	
+
 	@PostMapping("/admin/registerRoom")
 	public String registerRoomAction(Room room) {
-		
-		//값 넘어온거 확인
-		//System.out.println(room.toString());
-		//등록
-		
-		
+
+		// 값 넘어온거 확인
+		System.out.println(room.toString());
+		// 등록
+		int result = roomService.saveRoom(room);
+		System.out.println(result);
+
 		return "admin/registerRoom";
 	}
-	
-	
-	
-	//고객 관리/등록
+
+	// 고객 관리/등록
 }
