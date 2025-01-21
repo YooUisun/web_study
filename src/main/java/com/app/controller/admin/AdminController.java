3f3f914c480057b07180d3c0a3757ab96c1ae712
YooUisun <yoouisun33@naver.com>
2025-01-21 12:48:05 +0900

    관리자 페이지 사용자 추가하기 & JDBC NULL 처리 설정

--- src/main/java/com/app/controller/admin/AdminController.java
+++ src/main/java/com/app/controller/admin/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.app.dto.room.Room;
+import com.app.dto.user.User;
 import com.app.service.room.RoomService;
 import com.app.service.user.UserService;
 
@@ -35,28 +36,45 @@ public class AdminController {
 		// 등록
 		int result = roomService.saveRoom(room);
 		System.out.println(result);
-		
-		if(result >0 ) {
+
+		if (result > 0) {
 			return "redirect:/admin/rooms";
 		} else {
 			return "admin/registerRoom";
 		}
 
 	}
-	
-	//관리자 객실 목록 확인
+
+	// 관리자 객실 목록 확인
 	@GetMapping("/admin/rooms")
 	public String rooms(Model model) {
-			List<Room> roomList = roomService.findRoomList();
-			//service.findRoomList  호출 -> DAO findRoomList -> DB (Mybatos mapper) select
+		List<Room> roomList = roomService.findRoomList();
+		// service.findRoomList 호출 -> DAO findRoomList -> DB (Mybatos mapper) select
 //									<- List<Room>			<- List<Room>
 //Controller DB 로 부터 조회 데이터 -> 화면 전달 -> 화면 출력(표시)
-	
-	model.addAttribute("roomList", roomList);
-	
-	return "admin/rooms";
-	
+
+		model.addAttribute("roomList", roomList);
+
+		return "admin/rooms";
+
 	}
 
 	// 고객 관리/등록
+
+	@GetMapping("/admin/users/add")
+	public String addUser() {
+		return "admin/addUser";
+	}
+	
+	@PostMapping("/admin/users/add")
+	public String addUserAction(User user) {
+		
+		//사용자 추가 (관리자X)
+		user.setUserType("CUS");
+		int result = userService.saveUser(user);
+//		int result = userService.saveCustomerUser(user);
+		System.out.println("사용자 추가 처리 결과: " + result);
+		return "admin/addUser";
+
+	}
 }
