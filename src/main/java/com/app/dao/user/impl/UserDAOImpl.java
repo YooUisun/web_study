c68c874b2de1a595ec77793c445405fd5681eb54
YooUisun <yoouisun33@naver.com>
2025-01-23 16:55:26 +0900

    검색 조건에 따라서 사용자 정보 조회 하기 && mybatis 동적 쿼리

--- src/main/java/com/app/dao/user/impl/UserDAOImpl.java
+++ src/main/java/com/app/dao/user/impl/UserDAOImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import com.app.dao.user.UserDAO;
 import com.app.dto.user.User;
+import com.app.dto.user.UserSearchCondition;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
@@ -54,5 +55,12 @@ public class UserDAOImpl implements UserDAO {
 		
 		return result;
 	}
+
+	@Override
+	public List<User> findUserListBySearchCondition(UserSearchCondition userSearchCondition) {
+		
+		return null;
+	}
+
 
 }
